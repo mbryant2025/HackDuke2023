de7f8016e1b9889ad173dedfc875c15b3fd51219
mbryant2025 <mbryant2025@gmail.com>
2023-09-09 19:03:50 -0400

    flask

--- vision.py
+++ vision.py
@@ -1,96 +1,72 @@
-# This script uses the USB camera to detect movement and saves the raw image and movement image to the images folder
-
-
+from flask import Flask, render_template, Response
 import cv2
-import json
+
+app = Flask(__name__)
 
 MOTION_MIN = 3
-"""Motion threshold. If the average pixel value of the diff image is greater than this value, then there was significant motion"""
-
-cap = cv2.VideoCapture(0)
-
-
-def get_frame():
-    _, img = cap.read()
-
-    return img
 
 def frame_diff(frame1, frame2):
     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
     diff = cv2.absdiff(frame1, frame2)
-
     return diff
 
-def get_temp():
-    # TODO
-    # Fake temperature reading (farhenheit)
-    # Return random number between 50 and 120
-    return 69
-
-
 def highlight_motion(frame, diff):
-    # Overlay the diff image onto the original image with the diff in red
-    
-    # Make a copy of the original image
     frame_copy = frame.copy()
-
-    # Make a mask of the diff image
     _, mask = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
-
-    # Apply the mask to the copy of the original image
-    frame_copy[mask > 230] = [0,0,255]
-
+    frame_copy[mask > 230] = [0, 0, 255]
     return frame_copy
 
-
 def detect_motion(frame):
-     # Inputs a diff frame and determines if there was significant motion
-     
-     # Calculate the average pixel value of the diff image
     avg = cv2.mean(frame)[0]
-
-    # If the average pixel value is greater than MOTION_MIN, then there was significant motion
     if avg > MOTION_MIN:
         return True
-    
     return False
 
 
-def trigger_motion(motion):
-     # Write to the json file that motion was detected
-     # File is "./data/motion.json"
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    prev_frame = cap.read()[1]
+    motion_currently_detected = False
 
-     with open("./data/motion.json", "w") as f:
-        json.dump({"motion": motion}, f)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
 
-
-def main():
-      
-      prev_frame = get_frame()
-      motion_currently_detected = False
-
-      while 1:
-            
-            frame = get_frame()
-            
+            # Do motion detection
             diff = frame_diff(prev_frame, frame)
-
             diff_red = highlight_motion(frame, diff)
 
-            cv2.imshow("Image", diff_red)
+            # Update previous frame
+            prev_frame = frame.copy()
 
             d = detect_motion(diff)
 
-            # If motion_currently_detected is different than d, we need to update the json file
+            # if d:
+            #     print("Motion detected!")
+
             if motion_currently_detected != d:
-                trigger_motion(d)
                 motion_currently_detected = d
 
 
-            prev_frame = frame
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                  break
-main()
+
+            # Encode the frame as JPEG
+            ret, buffer = cv2.imencode('.jpg', diff_red)
+            if not ret:
+                break
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
